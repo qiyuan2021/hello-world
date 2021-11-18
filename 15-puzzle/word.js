95824b880fe1b41f5dc561fa855642ae8fcbef01
장기원 <zhqiyuan@naver.com>
2021-11-19 00:43:41 +0900

    퍼즐구현 3: 알파벳을 랜덤으로 섞기
    "일치합니다" 여부를 판단하는 함수 추가함, 뒤집기 버튼을 추가함.
    10개 정도의 Five Letters를 랜덤으로 뽑아서 보여주기 추가함.
    알파벳의 위치를 업데이트 할 때 마다 랜덤으로 배열하는 기능을 추가함.

--- 15-puzzle/word.js
+++ 15-puzzle/word.js
@@ -1,13 +1,33 @@
-var str = document.getElementById("word1").innerHTML;
+var word1 = document.getElementById("word1");
 var word2 = document.getElementById("word2");
+var check = document.getElementById('check');
+
+var random_words = 'APPLE,LINUX,AUDLT,BEGIN,BRAIN,BUILD,CHAIR,CREAM,HELLO,LOVER'
+var R_words = random_words.split(',');
 
 var game = {};
-game.word = str.split('');
+game.choice = function () {
+    var idx = Math.floor(Math.random() * R_words.length);
+    return R_words[idx];
+}
+
+var answer = game.choice();
+word1.innerHTML = answer;
+
+game.word = answer.split('');
 game.btnOb = [];
 
-for (var i = 0; i < str.length; i++) {
+game.updateDisplay = function () {
+    if (answer === game.word.join('')) {
+        check.innerHTML = '일치합니다';
+    } else {
+        check.innerHTML = '일치하지 않습니다';
+    }
+}
+
+for (var i = 0; i < answer.length; i++) {
     var btn = document.createElement('button');
-    btn.innerHTML = str[i];
+    btn.innerHTML = answer[i];
     word2.appendChild(btn);
     game.btnOb.push(btn);
 }
@@ -18,21 +38,41 @@ game.copyBtnText = function () {
     }
 }
 
-var turnover = function (event) {
-    for (var i = 0; i < game.word.length; i++) {
-
+var turnover = function () {
+    var temp = [];
+    while (game.word.length != 0) {
+        var s = game.word.pop();
+        temp.push(s);
     }
+    game.word = temp;
+    game.copyBtnText();
+    game.updateDisplay();
 };
 
-var rshift = function (event) {
+var rshift = function () {
     var s = game.word.pop();
     game.word.unshift(s);
     game.copyBtnText();
+    game.updateDisplay();
 };
 
-var lshift = function (event) {
+var lshift = function () {
     var l = game.word.shift();
     game.word.push(l);
     game.copyBtnText();
+    game.updateDisplay();
 };
 
+// shuffle 기능
+
+var toggle = Math.floor(Math.random() * 2) === 0;
+
+if (toggle) {
+    turnover();
+}
+
+var n = Math.floor(Math.random() * answer.length);
+
+for (var i = 0; i < n; i++) {
+    rshift();
+}
