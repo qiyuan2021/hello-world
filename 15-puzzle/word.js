468845310e46891bd91c4d13edcdec4fe87faac8
장기원 <zhqiyuan@naver.com>
2021-11-20 20:01:47 +0900

    퍼즐 구현 4: 진행상황처리
    객체와 메소드를 이용하여 코드를 정리한다.(리팩토링)
    한문제를 맞추면 다음 문제를 출제한다.
    한문제를 맞출 때 마다 상단에 O 하나씩 추가한다.
    세문제를 맞추면 Thank you 표시함.

--- 15-puzzle/word.js
+++ 15-puzzle/word.js
@@ -1,78 +1,138 @@
-var word1 = document.getElementById("word1");
-var word2 = document.getElementById("word2");
-var check = document.getElementById('check');
+// HTML Element
+const word1 = document.getElementById("word1"); //answer
+const word2 = document.getElementById("word2"); //buttons
+const check = document.getElementById('check'); //word1 === word2
+const progress = document.getElementById('progress'); // progress check
 
-var random_words = 'APPLE,LINUX,AUDLT,BEGIN,BRAIN,BUILD,CHAIR,CREAM,HELLO,LOVER'
-var R_words = random_words.split(',');
+//game object 
+const game = {
+    'btnOb': [],
+    'maxPlay': 3,
+    'current': 0
+};
 
-var game = {};
-game.choice = function () {
-    var idx = Math.floor(Math.random() * R_words.length);
-    return R_words[idx];
+const random_words = 'APPLE,LINUX,AUDLT,BEGIN,BRAIN,BUILD,CHAIR,CREAM,HELLO,LOVER'
+game.R_words = random_words.split(',');
+
+//choose 1 word from R_words
+game.choose = function () {
+    const idx = Math.floor(Math.random() * this.R_words.length);
+    this.answer = this.R_words[idx];
+    this.letters = this.answer.split('');
+    word1.innerHTML = this.answer;
+};
+
+game.addButtons = function () {
+    for (let i = 0; i < this.letters.length; i++) {
+        const btn = document.createElement('button');
+        btn.innerHTML = this.letters[i];
+        word2.appendChild(btn);
+        this.btnOb.push(btn);
+    }
+};
+
+game.removeButtons = function () {
+    for (let i = 0; i < this.btnOb.length; i++) {
+        word2.removeChild(this.btnOb[i]);
+    }
+    this.btnOb = [];
+};
+
+game.completed = function () {
+    return this.answer === this.letters.join('');
 }
-
-var answer = game.choice();
-word1.innerHTML = answer;
-
-game.word = answer.split('');
-game.btnOb = [];
-
 game.updateDisplay = function () {
-    if (answer === game.word.join('')) {
+    if (this.completed()) {
         check.innerHTML = '일치합니다';
     } else {
         check.innerHTML = '일치하지 않습니다';
     }
-}
+};
 
-for (var i = 0; i < answer.length; i++) {
-    var btn = document.createElement('button');
-    btn.innerHTML = answer[i];
-    word2.appendChild(btn);
-    game.btnOb.push(btn);
-}
+game.init = function () {
+    this.choose();
+    this.addButtons();
+    this.updateDisplay();
+};
+game.init();
 
 game.copyBtnText = function () {
-    for (var i = 0; i < this.word.length; i++) {
-        this.btnOb[i].innerHTML = this.word[i];
+    for (let i = 0; i < this.letters.length; i++) {
+        this.btnOb[i].innerHTML = this.letters[i];
     }
 }
 
-var turnover = function () {
-    var temp = [];
-    while (game.word.length != 0) {
-        var s = game.word.pop();
+game.turnover = function () {
+    const temp = [];
+    //copy and turnover
+    while (game.letters.length != 0) {
+        const s = game.letters.pop();
         temp.push(s);
     }
-    game.word = temp;
+    game.letters = temp;
     game.copyBtnText();
     game.updateDisplay();
-};
-
-var rshift = function () {
-    var s = game.word.pop();
-    game.word.unshift(s);
-    game.copyBtnText();
-    game.updateDisplay();
-};
-
-var lshift = function () {
-    var l = game.word.shift();
-    game.word.push(l);
-    game.copyBtnText();
-    game.updateDisplay();
-};
-
-// shuffle 기능
-
-var toggle = Math.floor(Math.random() * 2) === 0;
-
-if (toggle) {
-    turnover();
 }
 
-var n = Math.floor(Math.random() * answer.length);
+game.rshift = function () {
+    const s = game.letters.pop();
+    game.letters.unshift(s);
+    game.copyBtnText();
+    game.updateDisplay();
+};
 
-for (var i = 0; i < n; i++) {
-    rshift();
-}
+game.lshift = function () {
+    const l = game.letters.shift();
+    game.letters.push(l);
+    game.copyBtnText();
+    game.updateDisplay();
+};
+
+game.progress = function () {
+    if (game.completed()) {
+        game.current++;
+        game.removeButtons();
+        game.init();
+        game.shuffle();
+        let str = "";
+        for (let i = 0; i < game.current; i++) {
+            str += "O";
+        }
+        progress.innerHTML = str;
+    }
+    if (game.current == game.maxPlay) {
+        alert("Good! Thank you for playing");
+    }
+};
+
+// event handler for 뒤집기 버튼
+const turnover = function () {
+    game.turnover();
+    game.progress();
+};
+
+// event handler for 왼쪽으로 밀기 버튼
+const rshift = function () {
+    game.rshift();
+    game.progress();
+};
+
+// event handler for 오른 쪽으로 밀기 버튼
+const lshift = function () {
+    game.lshift();
+    game.progress();
+};
+
+// 랜덤 배치 기능
+game.shuffle = function () {
+    const toggle = Math.floor(Math.random() * 2) === 0;
+    if (toggle) {
+        game.turnover();
+    }
+
+    const n = Math.floor(Math.random() * (game.answer.length - 1));
+    for (let i = 0; i < n; i++) {
+        game.rshift();
+    }
+};
+game.shuffle();
